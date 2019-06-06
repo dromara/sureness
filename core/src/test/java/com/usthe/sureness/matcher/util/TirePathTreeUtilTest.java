540da277bc1a124f4b83998d67dde69d44e46f5b
tomsun28 <tomsun28@outlook.com>
2019-06-06 16:38:05 +0800

    remove jwt

--- core/src/test/java/com/usthe/sureness/matcher/util/TirePathTreeUtilTest.java
+++ core/src/test/java/com/usthe/sureness/matcher/util/TirePathTreeUtilTest.java
@@ -31,16 +31,16 @@ public class TirePathTreeUtilTest {
     @Test
     public void buildTree() {
         Set<String> paths = new HashSet<>();
-        paths.add("/api/v2/host===post===jwt[role2,role3,role4]");
-        paths.add("/api/v2/host===get===jwt[role2,role3,role4]");
-        paths.add("/api/v2/host===delete===jwt[role2,role3,role4]");
-        paths.add("/api/v2/host===put===jwt[role2,role3,role4]");
-        paths.add("/api/v1/host===put===jwt[role2,role3,role4]");
-        paths.add("/api/v3/host===put===jwt[role2,role3,role4]");
-        paths.add("/api/v2/detail===put===jwt[role2,role3,role4]");
-        paths.add("/api/v2/mom===put===jwt[role2,role3,role4]");
-        paths.add("/api/*/mom/ha===put===jwt[role2,role3,role4]");
-        paths.add("/api/mi/**===put===jwt[role2,role3,role4]");
+        paths.add("/api/v2/host===post===[role2,role3,role4]");
+        paths.add("/api/v2/host===get===[role2,role3,role4]");
+        paths.add("/api/v2/host===delete===[role2,role3,role4]");
+        paths.add("/api/v2/host===put===[role2,role3,role4]");
+        paths.add("/api/v1/host===put===[role2,role3,role4]");
+        paths.add("/api/v3/host===put===[role2,role3,role4]");
+        paths.add("/api/v2/detail===put===[role2,role3,role4]");
+        paths.add("/api/v2/mom===put===[role2,role3,role4]");
+        paths.add("/api/*/mom/ha===put===[role2,role3,role4]");
+        paths.add("/api/mi/**===put===[role2,role3,role4]");
         TirePathTreeUtil.buildTree(paths, root);
         Assert.assertEquals(root.getChildren().size(), 1);
     }
@@ -49,19 +49,19 @@ public class TirePathTreeUtilTest {
     public void searchPathFilterRoles() {
         buildTree();
         String filterRole = TirePathTreeUtil.searchPathFilterRoles("/api/v2/host===get", root);
-        Assert.assertEquals(filterRole, "jwt[role2,role3,role4]");
+        Assert.assertEquals(filterRole, "[role2,role3,role4]");
         String var1 = TirePathTreeUtil.searchPathFilterRoles("/api/v1/mom===put", root);
         Assert.assertNull(var1);
         String var2 = TirePathTreeUtil.searchPathFilterRoles("/api/v2/host===put", root);
-        Assert.assertEquals(var2, "jwt[role2,role3,role4]");
+        Assert.assertEquals(var2, "[role2,role3,role4]");
         String var3 = TirePathTreeUtil.searchPathFilterRoles("/api/v2/detail===put", root);
-        Assert.assertEquals(var3, "jwt[role2,role3,role4]");
+        Assert.assertEquals(var3, "[role2,role3,role4]");
         String var4 = TirePathTreeUtil.searchPathFilterRoles("/api/v2/details===put", root);
         Assert.assertNull(var4);
         String var5 = TirePathTreeUtil.searchPathFilterRoles("/api/dd/mom/ha===put", root);
-        Assert.assertEquals(var5, "jwt[role2,role3,role4]");
+        Assert.assertEquals(var5, "[role2,role3,role4]");
         String var6 = TirePathTreeUtil.searchPathFilterRoles("/api/mi/mom/ha===put", root);
-        Assert.assertEquals(var6, "jwt[role2,role3,role4]");
+        Assert.assertEquals(var6, "[role2,role3,role4]");
 
     }
 }
