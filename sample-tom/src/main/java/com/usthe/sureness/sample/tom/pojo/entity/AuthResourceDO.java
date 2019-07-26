b895f0221b2bf58c448feab6fad5119f6ddc7d58
tomsun28 <tomsun28@outlook.com>
2019-07-27 00:31:44 +0800

    init data object

---
+++ sample-tom/src/main/java/com/usthe/sureness/sample/tom/pojo/entity/AuthResourceDO.java
@@ -0,0 +1,19 @@
+package com.usthe.sureness.sample.tom.pojo.entity;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * @author tomsun28
+ * @date 00:00 2019-07-26
+ */
+@Entity
+@Table(name = "auth_resource")
+@Data
+public class AuthResourceDO {
+
+
+
+}
---
+++ sample-tom/src/main/java/com/usthe/sureness/sample/tom/pojo/entity/AuthUserDO.java
@@ -0,0 +1,17 @@
+package com.usthe.sureness.sample.tom.pojo.entity;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * @author tomsun28
+ * @date 00:29 2019-07-27
+ */
+@Entity
+@Table(name = "auth_user")
+@Data
+public class AuthUserDO {
+
+}
