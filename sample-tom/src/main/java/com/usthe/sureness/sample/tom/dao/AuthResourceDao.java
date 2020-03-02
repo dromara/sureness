e280de27e2e9d3194be3938ccde62c992b8d8283
tomsun28 <tomsun28@outlook.com>
2020-03-03 00:59:09 +0800

    fin getPathRoleSql

--- sample-tom/src/main/java/com/usthe/sureness/sample/tom/dao/AuthResourceDao.java
+++ sample-tom/src/main/java/com/usthe/sureness/sample/tom/dao/AuthResourceDao.java
@@ -17,6 +17,22 @@ public interface AuthResourceDao extends JpaRepository<AuthResourceDO, Long> {
      * 获取uri资源与其对应角色关系链 eg: /api/v2/host===post===[role2,role3,role4]
      * @return 资源角色链set
      */
-//    @Query("select 1 from AuthResourceDO group by ")
-//    Optional<Set<String>> getPathRoleData();
+    @Query(value = "SELECT  CONCAT(LOWER(res.uri),\"===\",LOWER(res.method),\"===[\",IFNULL(GROUP_CONCAT(DISTINCT role.code),\"\"),\"]\")" +
+            "FROM auth_resource res" +
+            "LEFT JOIN auth_role_resource_bind bind on res.id = bind.resource_id" +
+            "LEFT JOIN auth_role role on role.id = bind.role_id" +
+            "where res.status = 1" +
+            "group by res.id", nativeQuery = true)
+    Optional<Set<String>> getEnableResourcePathRoleData();
+
+
+
+    /**
+     * 获取禁用的uri资源 eg: /api/v2/host===post
+     * @return 资源链set
+     */
+    @Query("select CONCAT(LOWER(resource.uri), \"===\", resource.method) " +
+            "from AuthResourceDO resource where resource.status = 9 order by resource.id")
+    Optional<Set<String>> getDisableResourcePathData();
+
 }
