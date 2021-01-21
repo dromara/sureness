c8c3c2a82451caf82b4ca9401f40b7d99edbf55a
tomsun28 <tomsun28@outlook.com>
2021-01-21 20:31:16 +0800

    fix xss clean

--- samples/spring-webflux-sureness/src/main/java/com/usthe/sureness/spring/webflux/controller/ExampleController.java
+++ samples/spring-webflux-sureness/src/main/java/com/usthe/sureness/spring/webflux/controller/ExampleController.java
@@ -26,6 +26,22 @@ public class ExampleController {
     @GetMapping("/auth/error")
     public ResponseEntity<String> authError(@RequestHeader(value = "statusCode") String statusCode,
                                                                @RequestHeader(value = "errorMsg") String errorMsg) {
-        return ResponseEntity.status(Integer.parseInt(statusCode)).body(errorMsg);
+        return ResponseEntity.status(Integer.parseInt(statusCode)).body(cleanXss(errorMsg));
+    }
+
+    /**
+     * xss clean
+     * @param value value
+     * @return clean value
+     */
+    private String cleanXss(String value) {
+        //You'll need to remove the spaces from the html entities below
+        value = value.replaceAll("<", "& lt;").replaceAll(">", "& gt;");
+        value = value.replaceAll("\\(", "& #40;").replaceAll("\\)", "& #41;");
+        value = value.replaceAll("'", "& #39;");
+        value = value.replaceAll("eval\\((.*)\\)", "");
+        value = value.replaceAll("[\\\"\\\'][\\s]*javascript:(.*)[\\\"\\\']", "\"\"");
+        value = value.replaceAll("script", "");
+        return value;
     }
 }
