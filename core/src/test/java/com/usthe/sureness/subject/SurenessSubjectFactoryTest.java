0ddbee8f59a1c29b1b199fcd97fd717a153c0a64
tomsun28 <tomsun28@outlook.com>
2021-01-24 19:17:55 +0800

    update test case

--- core/src/test/java/com/usthe/sureness/subject/SurenessSubjectFactoryTest.java
+++ core/src/test/java/com/usthe/sureness/subject/SurenessSubjectFactoryTest.java
@@ -4,6 +4,8 @@ import com.usthe.sureness.subject.creater.BasicSubjectServletCreator;
 import com.usthe.sureness.subject.creater.DigestSubjectServletCreator;
 import com.usthe.sureness.subject.creater.JwtSubjectServletCreator;
 import com.usthe.sureness.subject.creater.NoneSubjectServletCreator;
+import com.usthe.sureness.subject.support.NoneSubject;
+import com.usthe.sureness.subject.support.PasswordSubject;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -44,14 +46,16 @@ class SurenessSubjectFactoryTest {
     void createSubjects() {
         registerSubjectCreator();
         HttpServletRequest request = createNiceMock(HttpServletRequest.class);
-        expect(request.getHeader(AUTHORIZATION)).andReturn(BASIC + " "
+        expect(request.getHeader(AUTHORIZATION)).andStubReturn(BASIC + " "
                 + new String(Base64.getEncoder().encode("admin:admin".getBytes(StandardCharsets.UTF_8))));
-        expect(request.getRequestURI()).andReturn("/api/v1/book");
-        expect(request.getMethod()).andReturn("put");
-        expect(request.getRemoteHost()).andReturn("192.167.2.1");
+        expect(request.getRequestURI()).andStubReturn("/api/v1/book");
+        expect(request.getMethod()).andStubReturn("put");
+        expect(request.getRemoteHost()).andStubReturn("192.167.2.1");
         replay(request);
         List<Subject> subjects = subjectFactory.createSubjects(request);
         assertNotNull(subjects);
+        assertTrue(subjects.get(0) instanceof NoneSubject);
+        assertTrue(subjects.get(1) instanceof PasswordSubject);
         assertEquals(2, subjects.size());
         verify(request);
     }
