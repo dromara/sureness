f710856a4ee26cbd4ab4dbe9060848a4fd2ab89f
tomsun28 <tomsun28@outlook.com>
2021-02-18 11:13:19 +0800

    support digest,basic subject principalMap

--- core/src/main/java/com/usthe/sureness/subject/support/DigestSubject.java
+++ core/src/main/java/com/usthe/sureness/subject/support/DigestSubject.java
@@ -53,6 +53,9 @@ public class DigestSubject implements Subject {
     /** the Roles which can access this resource above-targetUri **/
     private List<String> supportRoles;
 
+    /** the map for principal, add your custom principal **/
+    private PrincipalMap principalMap;
+
     public DigestSubject() {}
 
     private DigestSubject(Builder builder) {
@@ -69,6 +72,7 @@ public class DigestSubject implements Subject {
         this.ownRoles = builder.ownRoles;
         this.targetUri = builder.targetUri;
         this.supportRoles = builder.supportRoles;
+        this.principalMap = builder.principalMap;
     }
 
     @Override
@@ -78,7 +82,7 @@ public class DigestSubject implements Subject {
 
     @Override
     public PrincipalMap getPrincipalMap() {
-        return null;
+        return principalMap;
     }
 
     @Override
@@ -162,6 +166,7 @@ public class DigestSubject implements Subject {
         private List<String> ownRoles;
         private String targetUri;
         private List<String> supportRoles;
+        private PrincipalMap principalMap;
 
         public Builder(String username, String response) {
             this.appId = username;
@@ -175,6 +180,7 @@ public class DigestSubject implements Subject {
             this.ownRoles = (List<String>) subject.getOwnRoles();
             this.targetUri = String.valueOf(subject.getTargetResource());
             this.supportRoles = (List<String>) subject.getSupportRoles();
+            this.principalMap = subject.getPrincipalMap();
         }
 
         public DigestSubject.Builder setAppId(String appId) {
@@ -239,6 +245,11 @@ public class DigestSubject implements Subject {
 
         public DigestSubject.Builder setSupportRoles(List<String> supportRoles) {
             this.supportRoles = supportRoles;
+            return this;
+        }
+
+        public DigestSubject.Builder setPrincipalMap(PrincipalMap principalMap) {
+            this.principalMap = principalMap;
             return this;
         }
 
