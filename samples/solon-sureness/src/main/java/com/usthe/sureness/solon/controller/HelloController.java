7aa320df81159bc9efe2a43bc1c369826e487bc9
tomsun28 <tomsun28@outlook.com>
2021-05-10 20:22:34 +0800

    simplify solon-sureness demo's api,auth data

--- samples/solon-sureness/src/main/java/com/usthe/sureness/solon/controller/HelloController.java
+++ samples/solon-sureness/src/main/java/com/usthe/sureness/solon/controller/HelloController.java
@@ -14,71 +14,43 @@ public class HelloController {
     public static final String SUCCESS_ACCESS_RESOURCE = "access this resource: {%s - %s} success";
 
     @Get
-    @Mapping("/api/v1/source1")
+    @Mapping("/api/v1/bar/{id}")
     public String api1Mock1() {
         Context context = ContextUtil.current();
         return String.format(SUCCESS_ACCESS_RESOURCE, context.method(), context.path());
     }
 
     @Post
-    @Mapping("/api/v1/source1")
+    @Mapping("/api/v1/bar")
     public String api1Mock2() {
         Context context = ContextUtil.current();
         return String.format(SUCCESS_ACCESS_RESOURCE, context.method(), context.path());
     }
 
     @Put
-    @Mapping("/api/v1/source1")
+    @Mapping("/api/v2/bar")
     public String api1Mock3() {
         Context context = ContextUtil.current();
         return String.format(SUCCESS_ACCESS_RESOURCE, context.method(), context.path());
     }
 
-    @Delete
-    @Mapping("/api/v1/source1")
+    @Get
+    @Mapping("/api/v2/foo")
     public String api1Mock4() {
         Context context = ContextUtil.current();
         return String.format(SUCCESS_ACCESS_RESOURCE, context.method(), context.path());
     }
 
-    @Get
-    @Mapping("/api/v2/host")
+    @Delete
+    @Mapping("/api/v2/foo")
     public String api1Mock5() {
         Context context = ContextUtil.current();
         return String.format(SUCCESS_ACCESS_RESOURCE, context.method(), context.path());
     }
 
-    @Post
-    @Mapping("/api/v2/host")
-    public String api1Mock6() {
-        Context context = ContextUtil.current();
-        return String.format(SUCCESS_ACCESS_RESOURCE, context.method(), context.path());
-    }
-
-    @Put
-    @Mapping("/api/v2/host")
-    public String api1Mock7() {
-        Context context = ContextUtil.current();
-        return String.format(SUCCESS_ACCESS_RESOURCE, context.method(), context.path());
-    }
-
-    @Delete
-    @Mapping("/api/v2/host")
-    public String api1Mock8() {
-        Context context = ContextUtil.current();
-        return String.format(SUCCESS_ACCESS_RESOURCE, context.method(), context.path());
-    }
-
     @Get
-    @Mapping("/api/v3/host")
-    public String api1Mock9() {
-        Context context = ContextUtil.current();
-        return String.format(SUCCESS_ACCESS_RESOURCE, context.method(), context.path());
-    }
-
-    @Put
-    @Mapping("/api/v3/book")
-    public String api1Mock0() {
+    @Mapping("/api/v3/foo")
+    public String api1Mock6() {
         Context context = ContextUtil.current();
         return String.format(SUCCESS_ACCESS_RESOURCE, context.method(), context.path());
     }
